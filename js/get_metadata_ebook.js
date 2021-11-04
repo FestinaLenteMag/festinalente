1c9b73ae9ed5fb6b8a52af3294f6ca693b9c3c42
Margherita Donelli <donellimargherita@gmail.com>
2021-11-04 18:19:26 +0100

    prova console log

--- js/get_metadata_ebook.js
+++ js/get_metadata_ebook.js
@@ -162,7 +162,7 @@
 			console.log(t)
 			$('html').animate({ scrollTop: t }, 200);
 			$(id).addClass('animate');
-			console.log(id)
+			console.log(id.innerHTML)
 			setTimeout(function(){
 				$(id).removeClass('animate');
 			},5000);
