ff9ef9c1dca5ee55776d693c1999296d692cf989
Margherita Donelli <donellimargherita@gmail.com>
2021-11-05 17:38:14 +0100

    errore ref

--- js/get_metadata_ebook.js
+++ js/get_metadata_ebook.js
@@ -161,7 +161,7 @@
 			$('#person-tab_a').click(function(){
 				$('.mention-person').addClass('text-person');
 				setTimeout(function(){
-				$(id).removeClass('text-person');
+				$(.mention-person).removeClass('text-person');
 					},5000)
 			});
 				
